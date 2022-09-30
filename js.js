d62222e5996daa9de1c1ff89225f8b8ccffed481
osamukuuun <eva2002guk@gmail.com>
2022-09-30 18:50:24 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+// 1 завдання
+// let n = +prompt("Введіть число n: ");
+// let sum = 0;
+// console.log("Number n:" + n);
+// for (let i = n; i > 0; i--){
+//     if (n % i == 0){
+//         sum+=i;
+//     }
+// }
+// console.log("sum" + sum);
+
+// 2 завдання
+// let a = +prompt("Enter number a: ");
+// let b = +prompt("Enter number b: ");
+// let count = 0;
+// console.log("number a: ");
+// console.log("number b: ");
+// for (let i = a; i <= b; i++) {
+//     if ( ((i**3 % 10) == 4)||((i**3 % 10) == 9) ){
+//         count++;        
+//     }
+// }
+// console.log("Count" + count);
